52db0fab4fec4ad7d664695c1c55f0b63163bf77
tottocoslowlifer <pho.slowlife@gmail.com>
2024-03-05 11:40:37 +0900

    Upload files

---
+++ scripts/Moon_convert.py
@@ -0,0 +1,6 @@
+import pandas as pd
+cnt = 2011
+for i in range(10):
+    dir = "../data/Moon_2011-2021"
+    data = pd.read_csv(dir + "/raw/mooncal" + str(cnt+i) + ".csv", encoding="shift-jis")
+    data.to_csv(dir + "/" + str(cnt+i) + ".csv")
