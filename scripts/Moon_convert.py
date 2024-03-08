664a4a4886994ac7026264f003ca3e3faee8028f
tottocoslowlifer <pho.slowlife@gmail.com>
2024-03-08 10:45:56 +0900

    Create files

--- scripts/Moon_convert.py
+++ scripts/Moon_convert.py
@@ -1,16 +1,6 @@
-import csv
 import os
 
 import pandas as pd
-
-
-def csv_integrater(filename, writer):
-    with open(filename) as f:
-        file = f.readlines()[1:]
-        for row in file:
-            row = row.rstrip()
-            read = row.split(",")
-            writer.writerow(read)
 
 
 def get_raw_files(dir_path) -> list:
@@ -19,33 +9,24 @@ def get_raw_files(dir_path) -> list:
 
     for i in range(11):
         name = dir_path + "/raw/mooncal" + str(cnt+i) + ".csv"
-        data = pd.read_csv(name, encoding="shift-jis", index_col=0)
-        data.to_csv(name, index=False)
-        filenames.append(name)
+        save_name = dir_path + "/raw/UTF-8_mooncal" + str(cnt+i) + ".csv"
+        data = pd.read_csv(name, encoding="shift-jis")
+        data.to_csv(save_name, index=False)
+        filenames.append(save_name)
     return filenames
 
 
 def main():
     raw_dir = "../data/Moon_2011-2021"
-
     csv_filename = raw_dir + "/Mooncal_2011-2021.csv"
 
     if os.path.isdir(raw_dir):
         raw_directories = get_raw_files(raw_dir)
-        with open(csv_filename, mode="w", newline="") as fw:
-            writer = csv.writer(fw)
-            writer.writerow(
-                [
-                    "月日",
-                    "黄経差",
-                    "月齢",
-                    "こよみ",
-                    "気象庁",
-                    "MIRC"
-                ]
-            )
-            for file in raw_directories:
-                csv_integrater(file, writer)
+        data = []
+        for item in raw_directories:
+            data.append(pd.read_csv(item))
+        all_df = pd.concat(data).reset_index().drop("index", axis=1)
+        all_df.to_csv(csv_filename, index=False)
 
     else:
         print("Exceptional Error:")
