1d7019cda93bbefefcb5c829cc1b9ce101e65ba4
tottocoslowlifer <pho.slowlife@gmail.com>
2024-04-29 20:41:13 +0900

    Update a file

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,21 +1,7 @@
-import configparser
-import os
-
 import pandas as pd
 
 
-def main():
-    config_ini = configparser.ConfigParser()
-    config_ini_path = "xxx.ini"  # 後ほど変更
-
-    if not os.path.exists(config_ini_path):
-        print("Exceptional Error:")
-        print(f"{config_ini_path} is not found")
-
-    config_ini.read(config_ini_path, encoding="utf-8")
-    df = pd.read_csv(config_ini["D/S"])  # 後ほど変更
+def get_dataframe(cfg: dict) -> pd.DataFrame:
+    path = cfg["data_path"]
+    df = pd.read_csv(path)
     return df
-
-
-if __name__ == '__main__':
-    main()
